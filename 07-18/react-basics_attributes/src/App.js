df6fedff64a5cb278d7035ad8d2338334541df6d
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-07-18 11:34:21 +0200

    add challenges 07-18

---
+++ 07-18/react-basics_attributes/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article className="article>">
+      <h2 className="article_title">Hello this is my h2</h2>
+      <label htmlFor="input_id"></label>
+      <input id="input_id"></input>
+      <a
+        className="article_link"
+        href="https://en.wikipedia.org/wiki/Wikipedia"
+      >
+        Yeaaaaaah!
+      </a>
+    </article>
+  );
+}
