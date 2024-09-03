b0baa02b86410f736290d851a8db7069be141292
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-03 12:37:56 +0200

    add styled component and font

---
+++ 08-28/lotr-app/components/DetailPageStyles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { volumes } from "@/lib/data";
+
+export default function BookStyle() {
+  return <BookStyle color={color}></BookStyle>;
+}
+
+const BookStyling = styled.ul`
+  background-color:  
+`;
+#ff49d1
