fba572c8408e08ed040c1dd7edc6c2a7b7518b7d
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-05 11:39:16 +0200

    add card functionality

---
+++ 09-05/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,49 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+async function fetcher(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  } else {
+    const data = await response.json();
+    return data;
+  }
+}
+
+export default function Character() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, error } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error || !data) {
+    return <h1>Error: {error}</h1>;
+  }
+
+  return (
+    <Layout>
+      <Card
+        id={data.id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_Color}
+        birthYear={data.birth_Year}
+      />
+    </Layout>
+  );
+}
