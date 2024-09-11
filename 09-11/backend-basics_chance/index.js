47d1c7a2dfdca7396c74a10e653b0fcac596541d
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-11 12:06:54 +0200

    Revert "add challenges 09-11"
    This reverts commit d4f7379e130e0635c0306082f5f1c58c0cb13048.

--- 09-11/backend-basics_chance/index.js
+++
@@ -1,7 +0,0 @@
-// import { server } from "./server.js";
-import { server } from "./server.js";
-// server.listen(…
-const port = 8000;
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
--- 09-11/backend-basics_greeting/index.js
+++
@@ -1,8 +0,0 @@
-// import { server } from "./server.js";
-import { server } from "./server.js";
-
-// server.listen(…
-const port = 8000;
-server.listen(port, () => {
-  console.log(`Server running at http://127.0.0.1:${port}/`);
-});
