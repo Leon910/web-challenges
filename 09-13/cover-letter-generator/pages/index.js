54c7275e63fc6d0e4796273352e97339d3c0b654
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-13 10:10:45 +0200

    adjust styling of form

--- 09-13/cover-letter-generator/pages/index.js
+++ 09-13/cover-letter-generator/pages/index.js
@@ -2,21 +2,26 @@ import styled from "styled-components";
 
 export default function HomePage() {
   return (
-    <StyledForm>
-      <StyledFieldset>
-        <label htmlFor="personal-info">Enter a few words about yourself</label>
-        <input type="text" id="personal-info" />
-        <label htmlFor="job-description">Enter the job description</label>
-        <input type="text" id="job-description" />
-        <button type="submit">Generate</button>
-        <button type="submit">Reset</button>
-      </StyledFieldset>
-    </StyledForm>
+    <>
+      <StyledForm>
+        <StyledFieldset>
+          <h2>Cover Letter Generator</h2>
+          <label htmlFor="personal-info">
+            Enter a few words about yourself
+          </label>
+          <StyledInput type="text" id="personal-info" />
+          <label htmlFor="job-description">Enter the job description</label>
+          <StyledInput type="text" id="job-description" />
+          <button type="submit">Generate</button>
+          <button type="submit">Reset</button>
+        </StyledFieldset>
+      </StyledForm>
+    </>
   );
 }
 
 const StyledForm = styled.form`
-  margin: 60px;
+  margin-top: 60px;
 `;
 
 const StyledFieldset = styled.fieldset`
@@ -24,4 +29,9 @@ const StyledFieldset = styled.fieldset`
   flex-wrap: wrap;
   flex-direction: column;
   align-items: start;
+`;
+
+const StyledInput = styled.input`
+  width: 200px;
+  height: 200px;
 `;
