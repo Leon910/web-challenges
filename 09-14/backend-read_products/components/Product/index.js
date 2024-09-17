8d889c22790e4b0feaebf9fe333a19218ae89341
Leon Fricke-Kleefisch <leonkleefisch@gmail.com>
2024-09-17 12:15:06 +0200

    add challenge 09-17

---
+++ 09-14/backend-read_products/components/Product/index.js
@@ -0,0 +1,36 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import { StyledLink } from "../Link/Link.styled";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading } = useSWR(`/api/products/${id}`);
+
+  console.log(data);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      <h2>{data.name}</h2>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.map((review) => {
+        return <p key={review._id}>Reviews: {review.title}</p>;
+      })}
+
+      <StyledLink href="/">Back to all</StyledLink>
+    </ProductCard>
+  );
+}
